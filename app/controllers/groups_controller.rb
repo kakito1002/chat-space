6a301de6ae89825be7de7a4abf2a327ea8bbec47
kakito1002 <kakihara.amanpapa1002@gmail.com>
2019-09-18 11:47:15 +0900

    group.view-create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,15 @@
 class GroupsController < ApplicationController
-end
+    def new
+        @group = Group.new
+        @group.users << current_user
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update  
+    end
+end 
