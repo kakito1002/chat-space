181cbe305d2335b16b2751fd778991ad9f043d92
kakito1002 <kakihara.amanpapa1002@gmail.com>
2019-09-23 12:12:34 +0900

    organize.code

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,14 +7,13 @@ class MessagesController < ApplicationController
     end
   
     def create
-      # binding.pry
       @message = @group.messages.new(message_params)
       if @message.save
         respond_to do |format|
           format.html { redirect_to "group_messages_path(params[:group_id])", notice: 'メッセージが送信されました' }
           format.json
-      end
-      else
+        end
+       else
         @messages = @group.messages.includes(:user)
         flash.now[:alert] = 'メッセージを入力してください。'
         render :index
