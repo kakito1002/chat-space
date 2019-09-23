fd0b1f1a2ff68ce607833f7a7d74c99b4ea188b2
kakito1002 <kakihara.amanpapa1002@gmail.com>
2019-09-23 20:06:07 +0900

    ajax.create

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,39 @@
 $(function() {
-    $(".search__query").on("keyup", function() {
+ var search_user_list = $("#user-search-result");
+ var add_user_list = $(".chat-group-users.js-add-user");
+    
+      function appendUserId(users_id) {
+        $('.chat-group-user.clearfix.js-chat-member').each(function(){
+          var user_id = $(this).attr('id');
+          users_id.push(user_id);
+        });
+        return users_id;
+      }
+    
+
+
+    $(".chat-group-form__input").on("keyup", function() {
       var input = $(".search__query").val();
-      console.log(input);
+      var users_id = [];
+      appendUserId(users_id);
+
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: { keyword: input, group_users_id: users_id },
+        dataType: 'json'
+      })
+
+      .done(function(users) {
+        $("#user-search-result").empty();
+          if (users.length !== 0) {
+            users.forEach(function(user){
+              appendUser(user);
+            });
+          }
+          else {
+            appendErrorMessageToHTML("一致するユーザーが見つかりません");
+          }
+      })
     });
   });
