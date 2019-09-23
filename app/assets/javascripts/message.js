902ba22b24971cc8015056ef031e14de6f0cc790
kakito1002 <kakihara.amanpapa1002@gmail.com>
2019-09-23 09:58:46 +0900

    html.create

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,22 +2,24 @@ $(document).on('turbolinks:load', function(){
     function buildHTML(message){
       var content = message.content ? `${ message.content }` : "";
       var img = message.image ? `<img src= ${ message.image }>` : "";
-      var html = `<div class = "message" data-id=${message.id}>
-                   <div class="message__upper-info">
-                     <p class = "message__upper-info__user-name">
-                       ${message.user_name}
-                     </p>
-                     <p class = "message__upper-info__date">
-                       ${message.date}
-                     </p>
-                   </div>
-                   <div class = "message__lower-message">
-                     <p class = "lower-message__content">
-                       ${content}
-                     </p>
-                     ${img}
-                   </div>
-                 </div>`
+      var html =  `<div class="message" data-id=${message.id}>
+                    <div class="upper-info">
+                      <p class="upper-info__user-name">
+                        ${message.user_name}
+                      </p>
+                      <p class="upper-info__date">
+                        ${message.date}
+                      </p>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                        ${content}
+                      </p>
+                      <img class="lower-message__image">
+                        ${img}
+                      </img>
+                    </div>
+                  </div>`
       return html;
     }
 
@@ -36,8 +38,9 @@ $(document).on('turbolinks:load', function(){
       .done(function(data){
         var html = buildHTML(data);
         $('.messages').append(html);
-        $('#message_content').val('');
+        $('#message_content').val(''); 
         scrollBottom();
+        //スクロール
         function scrollBottom(){
           var target = $('.message').last();
           var position = target.offset().top + $('.messages').scrollTop();
@@ -46,11 +49,13 @@ $(document).on('turbolinks:load', function(){
           }, 300, 'swing');
         }
       })
-      .fail(function(){
-        alert('error');
-      })
       .always(function(){
-        $('.submit-btn').prop('disabled', false);　//ここで解除している
+        $('.submit-btn').prop('disabled', false); //キャンセルコード
+      })
+      .fail(function(){
+        alert('エラーが発生しました');
       })
     })
 });
+
+
