4add824ead4a8f6e8d0f26f50b23d8aab8322bf2
kakito1002 <kakihara.amanpapa1002@gmail.com>
2019-09-24 14:43:16 +0900

    html.create

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,15 @@
 $(function() {
-    $(".user-search-field").on("keyup", function() {
-      var input = $(".user-search-field").val();
+
+    function appendUser(user) {
+        var html = `<div class="chat-group-user clearfix">
+                       <p class="chat-group-user__name">${ user.name }</p>
+                       <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
+                    </div>`
+        search_user_list.append(html);
+    }
+
+    $(".chat-group-form__input").on("keyup", function() {
+      var input = $(".chat-group-form__input.user-search-field").val();
       
       $.ajax({
         type: 'GET',
@@ -10,14 +19,14 @@ $(function() {
       })
   
       .done(function(users) {
-        $("#user-search-result").empty();
+        $(".user-search-result").empty();
         if (users.length !== 0 && input.length !== 0) {
           users.forEach(function(user){
             appendUser(user);
           });
         }
         else if (input.length == 0) {
-          $("#user-search-result").find('.chat-group-user.clearfix').remove();
+          $(".user-search-result").find('.chat-group-user.clearfix').remove();
         }
         else {
           appendErrorMessageToHTML("一致するユーザーが見つかりません");
